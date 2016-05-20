5bad6b7915b2d840d6a949474198594530cdf6e4
dupontbertrand <dupontbertrand59@gmail.com>
2016-05-20 16:00:31 +0200

    seeds edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Registration.delete_all
 User.delete_all
 User.create!(
     email: 'admin@admin.com',
